f9823158522026f4627025101640d1a7f1e3b166
Leafguyk <81000589+Leafguyk@users.noreply.github.com>
2021-12-08 08:53:42 +0900

    Update discord_paginator.py

--- discord_paginator/discord_paginator.py
+++ discord_paginator/discord_paginator.py
@@ -11,8 +11,8 @@ class Page:
     def __init__(self,
                  message : Union[SlashCommand,Message],
                  bot : Bot,
-                 embeds : Optional[List[Embed]],
-                 content : Optional[List[str]],
+                 embeds : Optional[List[Embed]] = None,
+                 content : Optional[List[str]] = None,
                  timeout : Optional[int] = None,
                  hidden : Optional[bool] = False,
                  skipper : Optional[bool] = False):
@@ -25,6 +25,12 @@ class Page:
         self.skipper=skipper
         if len(self.embeds)>len(self.content):
             self.length=len(self.embeds)
+            while not len(self.embeds)==len(self.content):
+                self.content.append(None)
+        elif len(self.embeds)<len(self.content):
+            self.length=len(self.content)
+            while not len(self.embeds)==len(self.content):
+                self.embeds.append(None)
         else:
             self.length=len(self.content)
     async def send(self):
@@ -33,25 +39,13 @@ class Page:
             self.components=[create_actionrow(create_button(label="Back",style=ButtonStyle.gray,custom_id="Back",disabled=True),
                                             create_button(label="Page "+str(self.count)+"/"+str(self.length),disabled=True,style=ButtonStyle.gray),
                                             create_button(label="Next",custom_id="Next",style=ButtonStyle.gray))]
-            try:
-                self.content[self.count]
-                try:
-                    self.embeds[self.count]
-                    msg = await self.message.send(content=self.content[self.count],embed=self.embeds[self.count],components=self.components,hidden=self.hidden)
-                except:
-                    msg = await self.message.send(content=self.content[self.count],components=self.components,hidden=self.hidden)
-            except:
-                try:
-                    self.embeds[self.count]
-                    msg = await self.message.send(embed=self.embeds[self.count],components=self.components,hidden=self.hidden)
-                except:
-                    None
+            msg = await self.message.send(content=self.content[self.count-1],embed=self.embeds[self.count-1],components=self.components,hidden=self.hidden)
             while True:
                 try:
-                    self.button_ctx : wait_for_component(client=self.bot,messages=msg,components=self.components,timeout=self.timeout)
-                    if self.button_ctx.custom_id=="Next":
+                    button_ctx : wait_for_component(client=self.bot,messages=msg,components=self.components,timeout=self.timeout)
+                    if button_ctx.custom_id=="Next":
                         self.count=self.count+1
-                    elif self.button_ctx.custom_id=="Back":
+                    elif button_ctx.custom_id=="Back":
                         self.count=self.count-1
                     if self.count==1:
                         self.components = [create_actionrow(create_button(label="Back",style=ButtonStyle.gray,custom_id="Back",disabled=True),
@@ -65,7 +59,7 @@ class Page:
                         self.components = [create_actionrow(create_button(label="Back",style=ButtonStyle.gray,custom_id="Back"),
                                                           create_button(label="Page "+str(self.count)+"/"+str(self.length),disabled=True,style=ButtonStyle.gray),
                                                           create_button(label="Next",custom_id="Next",style=ButtonStyle.gray))]
-                    await self.button_ctx.edit_origin(content=self.content[self.count],embed=self.embeds[self.count],components=self.components)
+                    await button_ctx.edit_origin(content=self.content[self.count],embed=self.embeds[self.count],components=self.components)
                 except:
                     self.components = [create_actionrow(create_button(label="Back",style=ButtonStyle.gray,custom_id="Back",disabled=True),
                                                           create_button(label="Page "+str(self.count)+"/"+str(self.length),disabled=True,style=ButtonStyle.gray),
@@ -76,25 +70,13 @@ class Page:
             self.components=[create_actionrow(create_button(label="Back",style=ButtonStyle.gray,custom_id="Back",disabled=True),
                                             create_button(label="Page "+str(self.count)+"/"+str(self.length),disabled=True,style=ButtonStyle.gray),
                                             create_button(label="Next",custom_id="Next",style=ButtonStyle.gray))]
-            try:
-                self.content[self.count]
-                try:
-                    self.embeds[self.count]
-                    msg = await self.message.channel.send(content=self.content[self.count],embed=self.embeds[self.count],components=self.components,hidden=self.hidden)
-                except:
-                    msg = await self.message.channel.send(content=self.content[self.count],components=self.components,hidden=self.hidden)
-            except:
-                try:
-                    self.embeds[self.count]
-                    msg = await self.message.channel.send(embed=self.embeds[self.count],components=self.components,hidden=self.hidden)
-                except:
-                    None
+            msg = await self.message.channel.send(content=self.content[self.count-1],embed=self.embeds[self.count-1],components=self.components)
             while True:
                 try:
-                    self.button_ctx : wait_for_component(client=self.bot,messages=msg,components=self.components,timeout=self.timeout)
-                    if self.button_ctx.custom_id=="Next":
+                    button_ctx : wait_for_component(client=self.bot,messages=msg,components=self.components,timeout=self.timeout)
+                    if button_ctx.custom_id=="Next":
                         self.count=self.count+1
-                    elif self.button_ctx.custom_id=="Back":
+                    elif button_ctx.custom_id=="Back":
                         self.count=self.count-1
                     if self.count==1:
                         self.components = [create_actionrow(create_button(label="Back",style=ButtonStyle.gray,custom_id="Back",disabled=True),
@@ -108,7 +90,7 @@ class Page:
                         self.components = [create_actionrow(create_button(label="Back",style=ButtonStyle.gray,custom_id="Back"),
                                                           create_button(label="Page "+str(self.count)+"/"+str(self.length),disabled=True,style=ButtonStyle.gray),
                                                           create_button(label="Next",custom_id="Next",style=ButtonStyle.gray))]
-                    await self.button_ctx.edit_origin(content=self.content[self.count],embed=self.embeds[self.count],components=self.components)
+                    await button_ctx.edit_origin(content=self.content[self.count-1],embed=self.embeds[self.count-1],components=self.components)
                 except:
                     self.components = [create_actionrow(create_button(label="Back",style=ButtonStyle.gray,custom_id="Back",disabled=True),
                                                           create_button(label="Page "+str(self.count)+"/"+str(self.length),disabled=True,style=ButtonStyle.gray),
